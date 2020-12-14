81bf77e48c69db12e485ed7267d8f746d5cde563
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-12-14 14:26:35 +0100

    Add project files.

---
+++ Src/ConsoleDI/Application.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDI
+{
+    public class Application
+    {
+        private readonly IConfiguration configuration;
+
+        public Application(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Run(string[] args)
+        {
+            var message = configuration["Message"];
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Src/ConsoleDI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleDI
+{
+    class Program
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static IConfiguration Configuration { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, false);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureService(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            var scope = ServiceProvider.CreateScope();
+            var app = scope.ServiceProvider.GetRequiredService<Application>();
+            app.Run(args);
+            scope.Dispose();
+        }
+
+        private static void ConfigureService(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddScoped<Application>();
+        }
+    }
+}
